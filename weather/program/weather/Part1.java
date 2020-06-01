6e53f8ea02b5b0fda5011e3a05f1a18ddf83ead9
faseel <faseelm98@gmail.com>
2020-06-01 14:03:32 +0530

    test data

---
+++ weather/program/weather/Part1.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of Part1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.FileResource;
+import edu.duke.StorageResource;
+import edu.duke.*; 
+import org.apache.commons.csv.*;
+
+public class Part1 {
+    public void coldestHourFile(CSVParser parser, )
+}
